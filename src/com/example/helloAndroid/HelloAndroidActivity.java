61413ddeb660d32cec39169905ccbaf4299f2a71
Diablomarcus <diablomarcus@gmail.com>
2012-04-19 22:26:51 -0500

    Following proper layout standards now\!

--- src/com/example/helloAndroid/HelloAndroidActivity.java
+++ src/com/example/helloAndroid/HelloAndroidActivity.java
@@ -2,16 +2,14 @@ package com.example.helloAndroid;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class HelloAndroidActivity extends Activity {
-    /** Called when the activity is first created. */
+    
+	//Called when the activity is first created.
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        TextView tv = new TextView(this);
-        tv.setText("Hello World!");
-        setContentView(tv);
     }
 }
