55605e32f49ab9330f10f0626056d411c4117ec2
diablomarcus <diablomarcus@gmail.com>
2012-09-28 19:19:28 -0500

    Initial commit

---
+++ src/net/katerberg/hello/HelloWorldActivity.java
@@ -0,0 +1,42 @@
+package net.katerberg.hello;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class HelloWorldActivity extends Activity {
+
+
+    private OnClickListener saveClickListener = new OnClickListener() {
+    	public void onClick(View v) {
+    		TextView outputField = (TextView)findViewById(R.id.output);
+    		EditText inputField = (EditText)findViewById(R.id.input);
+    		
+    		String input = inputField.getEditableText().toString();
+    		String outputString="";
+    		
+    		if (!input.equals("")) {
+    			outputString = "You typed: "+input;
+    		} 
+    		outputField.setText(outputString);
+    		inputField.setText("");
+    		
+		}
+	};
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+ 
+        Button saveButton=(Button)this.findViewById(R.id.save);
+        saveButton.setOnClickListener(saveClickListener);
+        
+    }
+    
+}
