937c2eb6372261b08ebf7df077d6b554826f4c8e
diablomarcus <diablomarcus@gmail.com>
2012-11-04 19:20:01 -0600

    Stubbed out custom die class and placed a more interesting text view for user defined tab

---
+++ src/net/katerberg/tap/CustomDie.java
@@ -0,0 +1,26 @@
+package net.katerberg.tap;
+
+public class CustomDie {
+	private Integer maxValue;
+	private Integer modifier;
+	
+
+	public Integer getMaxValue() {
+		return maxValue;
+	}
+
+	public void setMaxValue(Integer maxValue) {
+		this.maxValue = maxValue;
+	}
+
+	public Integer getModifier() {
+		return modifier;
+	}
+
+	public void setModifier(Integer modifier) {
+		this.modifier = modifier;
+	}
+	
+	
+
+}
--- src/net/katerberg/tap/tabs/UserDefinedDiceTab.java
+++ src/net/katerberg/tap/tabs/UserDefinedDiceTab.java
@@ -1,14 +1,26 @@
 package net.katerberg.tap.tabs;
 
+import java.util.List;
+
+import net.katerberg.tap.CustomDie;
 import net.katerberg.tap.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class UserDefinedDiceTab extends Activity {
+	
+	List<CustomDie> customDiceList;
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.userdicetab);
+        
+        TextView result = (TextView)findViewById(R.id.diceDisplayView);
+        LinearLayout customDiceDisplayLayout= (LinearLayout)findViewById(R.id.customDiceRollsLayout);
+        result.setText("NOPE");
+        
     }
 }
