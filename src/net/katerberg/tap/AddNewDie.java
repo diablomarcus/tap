7a3af0cc525ae8f77cb42adcca092ad1189034d3
diablomarcus <diablomarcus@gmail.com>
2012-11-06 23:55:23 -0600

    Added toast

--- src/net/katerberg/tap/AddNewDie.java
+++ src/net/katerberg/tap/AddNewDie.java
@@ -5,35 +5,32 @@ import java.util.regex.Pattern;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.PatternMatcher;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddNewDie extends Activity{
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add_custom_die);
-
-		((Button)this.findViewById(R.id.acceptCustomDie)).setOnClickListener(createAcceptCustomDieListener());
+		EditText inputField = (EditText)findViewById(R.id.customDieInput);
+		((Button)this.findViewById(R.id.acceptCustomDie)).setOnClickListener(createAcceptCustomDieListener(inputField));
 	}
 
-	private OnClickListener createAcceptCustomDieListener() {
+	private OnClickListener createAcceptCustomDieListener(final EditText edit) {
 
 		OnClickListener acceptCustomDie = new OnClickListener() {
 
 			public void onClick(View v) {
-				EditText inputField = (EditText)v.findViewById(R.id.customDieInput);
-				String userInput = inputField.getText().toString();
+				
+				String userInput = edit.getText().toString();
 				CustomDie userDie = null; 
 				if(isInputValid(userInput)){
 					userDie = userInputToCustomDie(userInput);
 				}
-
-				
-				
 			}
 
 
@@ -48,12 +45,12 @@ public class AddNewDie extends Activity{
 				if (mathSeparator.length > 1){
 					die.setModifier(Integer.parseInt(mathSeparator[2]));
 				}
-				
+
 				if (dSeparator[0].contains("-"))
 				{
 					die.setModifier(die.getModifier()*-1);
 				}
-				
+
 				die.setMaxValue(typeOfDice);
 				die.setNumberOfDice(numberOfDice);
 				return die;
@@ -62,10 +59,14 @@ public class AddNewDie extends Activity{
 			private Boolean isInputValid(String input) {
 				Pattern validPattern = Pattern.compile("^[0-9]+d[0-9]+[+\\-]?[0-9]+?$");
 				Matcher matcher = validPattern.matcher(input);
+				if(!matcher.matches()){
+					Toast toast = Toast.makeText(getBaseContext(), "invalid input", Toast.LENGTH_SHORT);
+					toast.show();
+				}
 				return matcher.matches();
 			}
 		};
-		return null;
+		
+		return acceptCustomDie;
 	}
-
 }
