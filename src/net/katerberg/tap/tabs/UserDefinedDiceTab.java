284094ab9461fc5a9f5d7962469fff4b30d63bb2
diablomarcus <diablomarcus@gmail.com>
2012-11-11 15:07:41 -0600

    Stubbed out click listener for custom dice

--- src/net/katerberg/tap/tabs/UserDefinedDiceTab.java
+++ src/net/katerberg/tap/tabs/UserDefinedDiceTab.java
@@ -5,7 +5,7 @@ package net.katerberg.tap.tabs;
  * 
  * This file is part of TAP.
  * 
- * Katerproject is free software: you can redistribute it and/or modify
+ * TAP is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
@@ -18,19 +18,25 @@ package net.katerberg.tap.tabs;
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
+import helpers.CustomDiceHelper;
+
 import java.util.List;
 
 import beans.CustomDie;
 
 import net.katerberg.tap.AddNewDie;
 import net.katerberg.tap.R;
+import net.katerberg.tap.db.DbHandler;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.LinearLayout;
 
 public class UserDefinedDiceTab extends Activity {
 
@@ -40,9 +46,16 @@ public class UserDefinedDiceTab extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.user_dice_tab);
-
+		DbHandler dbHandler = new DbHandler(getApplicationContext());
+		
 		TextView result = (TextView)findViewById(R.id.diceDisplayView);
 		result.setText("NOPE");
+		LinearLayout customDice = (LinearLayout)findViewById(R.id.customDiceRollsLayout);
+		for (CustomDie customDie :  dbHandler.getAllCustomDice()){
+			Button button = new Button(this);
+			button.setOnClickListener(new CustomDiceHelper(customDie));
+			customDice.addView(button);
+		}
 	}
 
 	public boolean onCreateOptionsMenu(Menu menu) {
