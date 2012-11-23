3b94cc306c7d8fbfbcc4a70554bb08619b0aba9b
diablomarcus <diablomarcus@gmail.com>
2012-11-22 23:58:54 -0500

    Added custom dice logic. Still needs display fixed

--- src/net/katerberg/tap/listeners/AcceptCustomDieListener.java
+++ src/net/katerberg/tap/listeners/AcceptCustomDieListener.java
@@ -1,72 +1,74 @@
 package net.katerberg.tap.listeners;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+import net.katerberg.tap.TapApplication;
+import net.katerberg.tap.beans.Die;
+import net.katerberg.tap.db.DbHandler;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.Toast;
-import net.katerberg.tap.TapApplication;
-import net.katerberg.tap.beans.Die;
-import net.katerberg.tap.db.DbHandler;
 
 public class AcceptCustomDieListener implements OnClickListener {
 	DbHandler dbHandler;
-	EditText edit;
-	
-	public AcceptCustomDieListener(EditText editText) {
+	EditText nameEdit;
+	EditText sizeEdit;
+	EditText numberEdit;
+	EditText modifierEdit;
+
+	public AcceptCustomDieListener(EditText nameField, EditText typeField, EditText numberField, EditText modifierField) {
 		dbHandler = new DbHandler(TapApplication.getAppContext());
-		edit = editText;
+		nameEdit = nameField;
+		sizeEdit=typeField;
+		numberEdit=numberField;
+		modifierEdit=modifierField;
 	}
 
 	public void onClick(View v) {
-
-		String userInput = edit.getText().toString().toLowerCase();
-		Die userDie = null; 
-		if(isInputValid(userInput)){
-			userDie = userInputToCustomDie(userInput);
+		Integer numberVal, sizeVal;
+		String nameText;
+		
+		Integer modifierVal = getModifierValue(modifierEdit.getText().toString());
+		if (null == modifierVal){
+			return;
 		}
+		try{
+			nameText = nameEdit.getText().toString();
+			sizeVal = Integer.parseInt(sizeEdit.getText().toString());
+			numberVal= Integer.parseInt(numberEdit.getText().toString());
+		} catch (NumberFormatException e){
+			Toast toast = Toast.makeText(TapApplication.getAppContext(), "Invalid inputs", Toast.LENGTH_SHORT);
+			toast.show();
+			return;
+		}
+		Die userDie = new Die(nameText, numberVal, sizeVal, modifierVal);
 		if(userDie != null){
 			dbHandler.addCustomDie(userDie);
 			Toast toast = Toast.makeText(TapApplication.getAppContext(), "Roll Added", Toast.LENGTH_SHORT);
-			toast.show();
-			edit.setText("");
+			toast.show(	);
+			clearInputs();
 		}
 	}
 
-
-	private Die userInputToCustomDie(String userInput) {
-		Die die = new Die();
-		die.setModifier(0);
-		String[] dSeparator = userInput.split("d");
-		Integer numberOfDice=Integer.parseInt(dSeparator[0]);
-
-		String[] mathSeparator = dSeparator[1].split("[+-]");
-		Integer typeOfDice = Integer.parseInt(mathSeparator[0]);
-		if (mathSeparator.length> 1){
-			die.setModifier(Integer.parseInt(mathSeparator[1]));
-		}
-
-		if (!dSeparator[1].contains("+"))
-		{
-			die.setModifier(die.getModifier()*-1);
-		}
-
-		die.setMaxValue(typeOfDice);
-		die.setNumberOfDice(numberOfDice);
-		return die;
+	private void clearInputs() {
+		nameEdit.setText("");
+		numberEdit.setText("");
+		modifierEdit.setText("");
+		sizeEdit.setText("");
+		
 	}
 
-	private Boolean isInputValid(String input) {
-
-		Pattern validPattern = Pattern.compile("^[0-9]+d[0-9]+([+\\-][0-9]+)?$");
-
-		Matcher matcher = validPattern.matcher(input.trim());
-		if(!matcher.matches()){
-			Toast toast = Toast.makeText(TapApplication.getAppContext(), "Invalid Input", Toast.LENGTH_SHORT);
-			toast.show();
+	private Integer getModifierValue(String modifierString) {
+		if(null == modifierString || "" == modifierString || modifierString.length()==0){
+			return 0;
+		} else {
+		
+			try{
+				return Integer.parseInt(modifierString);
+			} catch (NumberFormatException e){
+				Toast toast = Toast.makeText(TapApplication.getAppContext(), modifierString, Toast.LENGTH_SHORT);
+				toast.show();
+				return null;
+			}
 		}
-		return matcher.matches();
 	}
 };
